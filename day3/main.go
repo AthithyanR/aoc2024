ffef7c55fb343fa647572576fb97665b2fbd01a6
AthithyanR <athithyanr4@gmail.com>
2024-12-08 10:47:57 +0530

    day3 complete

--- day3/main.go
+++ day3/main.go
@@ -1,14 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+// const fileName = "sample-input.txt"
+
+const fileName = "input.txt"
 
 func main() {
-	levels := []int{1, 2, 3, 4, 5}
+	fileBytes, _ := os.ReadFile(fileName)
+	file := string(fileBytes)
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)`)
+	matches := re.FindAllStringSubmatch(file, -1)
+	fmt.Println(matches)
 
-	for i := 0; i < len(levels); i++ {
-		newLevels := make([]int, len(levels)-1)
-		copy(newLevels, levels[:i])
-		copy(newLevels[i:], levels[i+1:])
-		fmt.Println(i, newLevels)
+	// sum := 0
+
+	// for _, match := range matches {
+	// 	num1, _ := strconv.Atoi(match[1])
+	// 	num2, _ := strconv.Atoi(match[2])
+	// 	sum += (num1 * num2)
+	// }
+
+	// fmt.Println(sum)
+
+	sum := 0
+	do := true
+
+	for _, match := range matches {
+		if match[0] == "do()" {
+			do = true
+		} else if match[0] == "don't()" {
+			do = false
+		} else {
+			if do {
+				num1, _ := strconv.Atoi(match[1])
+				num2, _ := strconv.Atoi(match[2])
+				sum += (num1 * num2)
+			}
+		}
 	}
+
+	fmt.Println(sum)
 }
