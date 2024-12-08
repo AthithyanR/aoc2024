c0ce01eefcc9a63865c396c51c412f56e957112b
AthithyanR <athithyanr4@gmail.com>
2024-12-08 12:48:41 +0530

    day4 pt2 complete

--- day4/main.go
+++ day4/main.go
@@ -29,40 +29,59 @@ func main() {
 	W := len(input[0])
 
 	// guard function
-	inBounds := func(row int, col int) bool {
-		return row >= 0 && row < H && col >= 0 && col < W
-	}
+	// inBounds := func(row int, col int) bool {
+	// 	return row >= 0 && row < H && col >= 0 && col < W
+	// }
 
-	// for each row & col
-	for row := 0; row < H; row++ {
-		for col := 0; col < W; col++ {
-			// if X
-			if input[row][col] == "X" {
-				// define directions & use them as delta
-				for dRow := -1; dRow <= 1; dRow++ {
-					for dCol := -1; dCol <= 1; dCol++ {
-						// skip self (not mandatory)
-						if dRow == 0 && dCol == 0 {
-							continue
-						}
+	// part 1
+	// // for each row & col
+	// for row := 0; row < H; row++ {
+	// 	for col := 0; col < W; col++ {
+	// 		// if X
+	// 		if input[row][col] == "X" {
+	// 			// define directions & use them as delta
+	// 			for dRow := -1; dRow <= 1; dRow++ {
+	// 				for dCol := -1; dCol <= 1; dCol++ {
+	// 					// skip self (not mandatory)
+	// 					if dRow == 0 && dCol == 0 {
+	// 						continue
+	// 					}
 
-						allGood := true
-						for i := 0; i < len(XMAS); i++ {
-							// cur + (delta * growth)
-							nRow := row + (dRow * i)
-							nCol := col + (dCol * i)
+	// 					allGood := true
+	// 					for i := 0; i < len(XMAS); i++ {
+	// 						// cur + (delta * growth)
+	// 						nRow := row + (dRow * i)
+	// 						nCol := col + (dCol * i)
 
-							if inBounds(nRow, nCol) && input[nRow][nCol] == string(XMAS[i]) {
-							} else {
-								allGood = false
-								break
-							}
-						}
+	// 						if inBounds(nRow, nCol) && input[nRow][nCol] == string(XMAS[i]) {
+	// 						} else {
+	// 							allGood = false
+	// 							break
+	// 						}
+	// 					}
 
-						if allGood {
-							count += 1
-						}
-					}
+	// 					if allGood {
+	// 						count += 1
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	pairs := [4][2]int{{-1, -1}, {-1, 1}, {1, 1}, {1, -1}}
+
+	// for each row & col (skip ones that could not have neighbors)
+	for row := 1; row < H-1; row++ {
+		for col := 1; col < W-1; col++ {
+			// if A
+			if input[row][col] == "A" {
+				str := ""
+				for _, pair := range pairs {
+					str += input[row+pair[0]][col+pair[1]]
+				}
+				if str == "MMSS" || str == "SSMM" || str == "SMMS" || str == "MSSM" {
+					count += 1
 				}
 			}
 		}
